81e215113c923e81d6dc5ab127c383b1531e00aa
Andrew0205-ai <andrewwork03295@gmail.com>
2025-11-09 13:35:01 +0800

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyClktI5_wSo-u9LuwdsBVzH6buizJPXMAs",
+  authDomain: "mycomment-ad1ba.firebaseapp.com",
+  projectId: "mycomment-ad1ba",
+  storageBucket: "mycomment-ad1ba.appspot.com",
+  messagingSenderId: "1076313273646",
+  appId: "1:1076313273646:web:2b5aaa8c6bd5824828f6bf",
+  measurementId: "G-3NGHCWH7TP"
+};
