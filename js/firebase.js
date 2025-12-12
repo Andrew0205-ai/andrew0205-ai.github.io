69ee433ec1a46e3787f609b888401c8663df9731
Andrew0205-ai <Andrew0205-ai@users.noreply.github.com>
2025-12-12 09:43:41 +0000

    deploy: ff676f909fb0923517c78bf10e9196f7675bab6a

--- js/firebase.js
+++ js/firebase.js
@@ -1,9 +1,14 @@
-// js/firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
-import { firebaseConfig } from "./firebase-config.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
+const firebaseConfig = {  apiKey: "AIzaSyClktI5_wSo-u9LuwdsBVzH6buizJPXMAs",
+  authDomain: "mycomment-ad1ba.firebaseapp.com",
+  projectId: "mycomment-ad1ba",
+  storageBucket: "mycomment-ad1ba.appspot.com",
+  messagingSenderId: "1076313273646",
+  appId: "1:1076313273646:web:2b5aaa8c6bd5824828f6bf",
+  measurementId: "G-3NGHCWH7TP"};
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
