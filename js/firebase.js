2a7eeb2fd863c20824bc2424ec4858257d52d2bd
Andrew0205-ai <andrewwork03295@gmail.com>
2025-12-17 14:44:46 +0800

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,17 +1,15 @@
-// ./js/firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-auth.js";
+import { initializeApp } from
+  "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyClktI5_wSo-u9LuwdsBVzH6buizJPXMAs",
-  authDomain: "mycomment-ad1ba.firebaseapp.com",
-  projectId: "mycomment-ad1ba",
-  storageBucket: "mycomment-ad1ba.appspot.com",
-  messagingSenderId: "1076313273646",
-  appId: "1:1076313273646:web:2b5aaa8c6bd5824828f6bf",
-  measurementId: "G-3NGHCWH7TP"
-};
+import { getAuth } from
+  "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 
-export const app = initializeApp(firebaseConfig);
+import { getFirestore } from
+  "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+
+import { firebaseConfig } from "./firebase-config.js";
+
+const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
 export const db = getFirestore(app);
