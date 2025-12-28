6811584270c1b59fb327b1f351073f943a402042
Andrew0205-ai <Andrew0205-ai@users.noreply.github.com>
2025-12-28 07:29:29 +0000

    deploy: 6e75f29f17ccf14147c3bff65504d30229a2733a

--- js/badge-index.js
+++ js/badge-index.js
@@ -1,18 +1,18 @@
   const avatar = document.getElementById("avatar");
 
-  let count = 0;
+  let count1 = 0;
   let timer = null;
 
   avatar.addEventListener("click", () => {
-    count++;
+    count1++;
 
-    if (count === 1) {
+    if (count1 === 1) {
       timer = setTimeout(() => {
         count = 0;
       }, 1500);
     }
 
-    if (count === 3) {
+    if (count1 === 3) {
       clearTimeout(timer);
       window.location.href = "tasks.html";
     }
