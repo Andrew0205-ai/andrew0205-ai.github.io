635ad92b5436c7efa4d75d3dbc87e0daab88f57b
Andrew0205-ai <andrewwork03295@gmail.com>
2026-01-10 17:58:22 +0800

    Create badge-index.js

---
+++ en/js/badge-index.js
@@ -0,0 +1,19 @@
+  const avatar = document.getElementById("avatar");
+
+  let count1 = 0;
+  let timer = null;
+
+  avatar.addEventListener("click", () => {
+    count1++;
+
+    if (count1 === 1) {
+      timer = setTimeout(() => {
+        count = 0;
+      }, 1500);
+    }
+
+    if (count1 === 3) {
+      clearTimeout(timer);
+      window.location.href = "../task/tasks.html";
+    }
+  });
